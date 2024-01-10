ca83bc24a7201e3efaae5ac082d148844756f624
André <AndreR@ppedv.de>
2024-01-10 10:09:23 +0100

    Hallo Singelton

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,37 @@
+﻿namespace HalloSingelton
+{
+    internal class Logger
+    {
+        private static Logger instance;
+        private static object sync = new object();
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (sync)
+                {
+                    if (instance == null)
+                        instance = new Logger();
+                }
+
+                return instance;
+            }
+        }
+
+        private Logger()
+        {
+            Info("Neue Logger instanz");
+        }
+
+        public void Info(string msg)
+        {
+            Console.WriteLine($"{DateTime.Now:g} [INFO]  {msg}");
+        }
+
+        public void Error(string msg)
+        {
+            Console.WriteLine($"{DateTime.Now:g} [ERROR] {msg}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using HalloSingelton;
+
+Console.WriteLine("Hello Singelton");
+//var log = new Logger();
+
+for (int i = 0; i < 10; i++)
+{
+    Task.Run(() => { Logger.Instance.Info($"Task {i}"); });
+}
+
+Logger.Instance.Info("Test 1");
+Logger.Instance.Info("Test 2");
+Logger.Instance.Info("Test 3");
+
+Logger.Instance.Error("Panik");
