9e7737062af0a269d4a7b595d4f5d84444e70afa
André <AndreR@ppedv.de>
2024-01-10 12:11:09 +0100

    Deco und observer

---
+++ HalloDecorator/HalloDecorator/IComponent.cs
@@ -0,0 +1,55 @@
+﻿namespace HalloDecorator
+{
+    interface IComponent
+    {
+        string Name { get; }
+        decimal Price { get; }
+    }
+
+    class Pizza : IComponent
+    {
+        public string Name => "Pizza";
+
+        public decimal Price => 6.9m;
+    }
+
+    class Brot : IComponent
+    {
+        public string Name => "Stulle";
+
+        public decimal Price => 3.5m;
+    }
+
+    abstract class Decorator : IComponent
+    {
+        protected IComponent parent;
+
+        protected Decorator(IComponent parent)
+        {
+            this.parent = parent;
+        }
+
+        public abstract string Name { get; }
+        public abstract decimal Price { get; }
+    }
+
+    class Salami : Decorator
+    {
+        public Salami(IComponent parent) : base(parent)
+        { }
+
+        public override string Name => parent.Name + " Salami";
+
+        public override decimal Price => parent.Price + 2.5m;
+    }
+
+    class Käse : Decorator
+    {
+        public Käse(IComponent parent) : base(parent)
+        { }
+
+        public override string Name => parent.Name + " Käse";
+
+        public override decimal Price => parent.Price + 1.7m;
+    }
+}
---
+++ HalloDecorator/HalloDecorator/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloDecorator;
+
+Console.WriteLine("Hello Decorator");
+
+
+var p1 = new Käse(new Salami(new Käse(new Pizza())));
+
+Console.WriteLine($"{p1.Name} {p1.Price}");
+
+
+var b1 = new Käse(new Salami(new Käse(new Brot())));
+
+Console.WriteLine($"{b1.Name} {b1.Price}");
+
+var p = new Party();
+p.Start += P_Start;
+p.Start += P_Start;
+p.Start += P_Start;
+p.Start += P_Start;
+p.Start -= P_Start;
+p.Start -= P_Start;
+
+void P_Start()
+{
+    Console.WriteLine("SAUFEN!!!");
+}
+
+p.StarteParty();
+
+
+class Party
+{
+    public event Action Start;
+
+    public void StarteParty()
+    {
+        Start.Invoke();
+    }
+}
