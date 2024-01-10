b48e3b10ec8de91d971eb3a3aa5c26a5f873fa1a
André <AndreR@ppedv.de>
2024-01-10 11:15:01 +0100

    Builder buildet

---
+++ HalloBuilder/HalloBuilder/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HalloBuilder;
+
+Console.WriteLine("Hello, World!");
+
+
+Schrank schrank = new Schrank.Builder()
+                             .SetTüren(4)
+                             .SetBöden(6)
+                             .Create();
+
+
+Schrank schrankBlau = new Schrank.Builder()
+                             .SetTüren(4)
+                             .SetBöden(6)
+                             .SetFarbe("blau")
+                             .Create();
+
+Console.WriteLine("Ende");
---
+++ HalloBuilder/HalloBuilder/Schrank.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace HalloBuilder
+{
+    internal class Schrank
+    {
+        private Schrank()
+        { }
+
+        public int AnzahlTüren { get; private set; }
+        public int AnzahlBöden { get; private set; }
+        public string Farbe { get; private set; }
+        public Oberfläche Oberfläche { get; private set; }
+
+        internal class Builder
+        {
+            Schrank schrank = new Schrank();
+            public Schrank Create() { return schrank; } 
+
+            public Builder SetTüren(int anzTüren)
+            {
+                if (anzTüren < 2 || anzTüren > 7)
+                    throw new ArgumentException("Zuviele oder zuwenig Türen :-O");
+
+                schrank.AnzahlTüren = anzTüren;
+
+                return this;
+            }
+
+            public Builder SetBöden(int anzBöden)
+            {
+                if (anzBöden < 0 || anzBöden > 6)
+                    throw new ArgumentException("Zuviele oder zuwenig Böden :-O");
+
+                schrank.AnzahlBöden = anzBöden;
+
+                return this;
+            }
+
+            public Builder SetFarbe(string farbe)
+            {
+                schrank.Oberfläche = Oberfläche.Lackiert;
+                schrank.Farbe = farbe;
+
+                return this;
+            }
+
+            public Builder SetOberfläche(Oberfläche oberfläche)
+            {
+                if (oberfläche == Oberfläche.Natur)
+                    schrank.Farbe = "";
+
+                schrank.Oberfläche = oberfläche;
+
+                return this;
+            }
+        }
+    }
+
+    internal enum Oberfläche
+    {
+        Natur,
+        Lackiert
+    }
+}
