e87f6277f9d7e90ab56cfad1234ba3920de67fda
André <AndreR@ppedv.de>
2024-01-11 09:31:15 +0100

    EfCore config

---
+++ ppedv.DiePizzaBude/ppedv.DiePizzaBude.Data.EfCore/PizzaEfContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.DiePizzaBude.Model;
+
+namespace ppedv.DiePizzaBude.Data.EfCore
+{
+    public class PizzaEfContext : DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Topping> Toppings { get; set; }
+
+        private string conString;
+
+        public PizzaEfContext(string conString)
+        {
+            this.conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(conString).UseLazyLoadingProxies(); 
+        }
+    }
+}
